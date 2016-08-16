3ed3c948981cc6f3fb6ed06e21c2a811fbc0baa4
Robby Kim <robert.st.kim@gmail.com>
2016-08-16 12:44:38 -0400

    started second exercise

--- complex.js
+++ complex.js
@@ -1,27 +1,34 @@
 // Finds the nth element in an array
-var findNthElement = function(array, n) {
-    if (array.length == 1) {
-        return array[n];
+var findNthElement = function(array, n, i) {
+    i = i || 0;
+
+    if (array.length == 1) { // happens once O(1)
+        return array[n]; // return happens once (maybe) O(1)
     }
 
-    var middle = Math.floor(array.length / 2);
-    if (n < middle) {
-        return findNthElement(array.slice(0, middle), n);
-    }
-    else {
-        return findNthElement(array.slice(middle, array.length), n - middle);
+    var middle = Math.floor(array.length / 2); // declaration happens once O(1)
+    if (n < middle) { // happens once O(1)
+        // console.log(i);
+        return findNthElement(array.slice(0, middle), n, i + 1); // loop O(log(n))
+    } else { // happens once O(1)
+        // console.log(i);
+        return findNthElement(array.slice(middle, array.length), n - middle, i + 1);
     }
 };
+
+// console.log(findNthElement([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20], 19));
 
 // Find a series of elements from an array
 var findElements = function(array, toFind) {
     var elements = [];
     for (var i=0; i<toFind.length; i++) {
-        var element = findNthElement(toFind[i]);
+        var element = findNthElement(array, toFind[i]);
         elements.push(element);
     }
     return elements;
 };
+
+console.log(findElements([1,2,3,4,5,6,7], [3,4,5]));
 
 // Returns whether a number is odd or even
 var isOdd = function(number) {
